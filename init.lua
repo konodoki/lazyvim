1a2599b3d29b6a72da388f58908d6a8bae2d2d14
konodoki <1326898804@qq.com>
2026-04-08 01:04:40 +0800

    修复剪切版

--- init.lua
+++ init.lua
@@ -1,14 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
-}
-vim.opt.clipboard = "unnamedplus"
 require("config.lazy")
